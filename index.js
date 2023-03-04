b25e3c71ca186b4a1db79d9f9543b48eb9f53027
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2023-03-05 01:58:04 +0530

    removed whitespace

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const rateLimit = require("express-rate-limit");
 const app = express();
 const port = process.env.PORT || 3000;
 dotenv.config();
+
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -16,17 +17,22 @@ app.use(express.json());
 const limiter = rateLimit({
   windowMs: process.env.MAX_REQUESTS_PER_MINUTE * 60 * 1000,
   max: process.env.MAX_REQUESTS,
-  message: `Too many requests, please try again in ${process.env.MAX_REQUESTS_PER_MINUTE} minutes`,
+  message: `Too many requests, you can only make ${process.env.MAX_REQUESTS} requests per ${process.env.MAX_REQUESTS_PER_MINUTE} minutes. Please try again later.`,
   standardHeaders: true,
 });
 
+const stringClean = (value) => {
+  return value.trim().replace(/\n{2,}/g, '\n');
+};
+
 const validateInput = (req, res, next) => {
-  const { dealingWithAI } = req.body;
-  if (!dealingWithAI) {
+  const { value } = req.body;
+  const question = stringClean(value);
+  if (!question) {
     res.status(400).send({ error: `String is empty` });
     return;
   }
-  const wordCount = dealingWithAI.split(/\s+/).length;
+  const wordCount = question.split(/\s+/).length;
   if (wordCount > process.env.MAX_WORDS) {
     res.status(400).send({ error: `String is too long (max ${process.env.MAX_WORDS} words)` });
     return;
@@ -36,16 +42,16 @@ const validateInput = (req, res, next) => {
 
 app.post("/v1/api/dealingWithAI", limiter, validateInput, async (req, res) => {
   try {
-    const { dealingWithAI } = req.body.trim();
+    const { value } = req.body;
+    const question = stringClean(value);
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${dealingWithAI}`,
+      prompt: `${question}`,
       max_tokens: 100,
     });
     const data = completion.data.choices[0].text.trim();
     res.send({ data });
   } catch (error) {
-    console.error(error);
     res.status(500).send({ error: "Something went wrong" });
   }
 });
